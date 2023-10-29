78f51661e1281966377931321b3977258f152d68
day070 <hidayatsalmanan25@gmail.com>
2023-10-29 20:46:47 +0800

    Menambahkan file detail-produk

--- admin/kategori-detail.php
+++ admin/kategori-detail.php
@@ -1,101 +1,104 @@
 <?php
-    require "session.php";
-    require "../koneksi.php";
-    $id = $_GET['q'];
-    $query = mysqli_query($con, "SELECT * FROM kategori WHERE id='$id'");
-    $data = mysqli_fetch_array($query);
+require "session.php";
+require "../koneksi.php";
+$id = $_GET['q'];
+$query = mysqli_query($con, "SELECT * FROM kategori WHERE id='$id'");
+$data = mysqli_fetch_array($query);
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detail Kategori</title>
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
 </head>
+
 <body>
     <?php require "navbar.php"; ?>
     <div class="container mt-5">
-    <div class="col-12 col-md-6">
-    <h2>Detail Kategori</h2>
+        <h2>Detail Kategori</h2>
+        <div class="col-12 col-md-6">
 
-        <form action="" method="post">
-            <div>
-                <label for="kategori">kategori</label>
-                <input type="text" name="kategori" id="kategori" value="<?php echo $data['nama']; ?>" class="form-control">
-            </div>
-            <div class="mt-5">
-                <button type="submit" class="btn btn-primary" name="editBtn">Edit</button>
-                <button type="submit" class="btn btn-danger" name="deleteBtn">Hapus</button>
-            </div>
-        </form>
-        <?php
-            if(isset($_POST['editBtn'])){
+            <form action="" method="post">
+                <div>
+                    <label for="kategori">kategori</label>
+                    <input type="text" name="kategori" id="kategori" value="<?php echo $data['nama']; ?>"
+                        class="form-control">
+                </div>
+                <div class="mt-5">
+                    <button type="submit" class="btn btn-primary" name="editBtn">Edit</button>
+                    <button type="submit" class="btn btn-danger" name="deleteBtn">Hapus</button>
+                </div>
+            </form>
+            <?php
+            if (isset($_POST['editBtn'])) {
                 $kategori = htmlspecialchars($_POST['kategori']);
-                if($data['nama']==$kategori){
+                if ($data['nama'] == $kategori) {
                     ?>
-                        <meta http-equiv="refresh" content="0; url=kategori.php"/>
+                    <meta http-equiv="refresh" content="0; url=kategori.php" />
                     <?php
-                }else{
+                } else {
                     $query = mysqli_query($con, "SELECT * FROM kategori WHERE nama='$kategori'");
                     $jumlahData = mysqli_num_rows($query);
 
-                    if($jumlahData>0){
+                    if ($jumlahData > 0) {
                         ?>
-                             <div class="alert alert-danger mt-3" role="alert">
-                                Kategori sudah ada!
-                            </div>
+                        <div class="alert alert-danger mt-3" role="alert">
+                            Kategori sudah ada!
+                        </div>
                         <?php
-                    }else{
+                    } else {
                         $querySimpan = mysqli_query($con, "UPDATE kategori SET nama='$kategori' WHERE id='$id'");
 
-                        if($querySimpan){
+                        if ($querySimpan) {
                             ?>
-                                <div class="alert alert-primary mt-3" role="alert">
+                            <div class="alert alert-primary mt-3" role="alert">
                                 Berhasil edit kategori!
-                                </div>
-                                <meta http-equiv="refresh" content="2; url=kategori.php"/>
+                            </div>
+                            <meta http-equiv="refresh" content="2; url=kategori.php" />
                             <?php
-                                
-                            }else{
-                                echo mysqli_error($con);
-                            }
+
+                        } else {
+                            echo mysqli_error($con);
+                        }
                     }
                 }
             }
 
-            if(isset($_POST['deleteBtn'])){
-               $queryCek = mysqli_query($con, "SELECT * FROM  produk WHERE kategori_id='$id'");
-               $dataCuy = mysqli_num_rows($queryCek);
-               $dataCuy = mysqli_num_rows($queryCek);
-               $queryDelete =mysqli_query($con, "DELETE  FROM kategori WHERE id='$id'");
-                if ($dataCuy>0) {
+            if (isset($_POST['deleteBtn'])) {
+                $queryCek = mysqli_query($con, "SELECT * FROM  produk WHERE kategori_id='$id'");
+                $dataCuy = mysqli_num_rows($queryCek);
+                $queryDelete = mysqli_query($con, "DELETE  FROM kategori WHERE id='$id'");
+                if ($dataCuy > 0) {
                     ?>
 
-                            <div class="alert alert-danger mt-3" role="alert">
-                                Gagal Terhapus Kategori Sedang Digunakan!
-                            </div>
+                    <div class="alert alert-danger mt-3" role="alert">
+                        Gagal Terhapus Kategori Sedang Digunakan!
+                    </div>
                     <?php
                     die();
                 }
-                if($queryDelete){
+                if ($queryDelete) {
                     ?>
-                                <div class="alert alert-primary mt-3" role="alert">
-                                Terhapus!
-                                </div>
+                    <div class="alert alert-primary mt-3" role="alert">
+                        Terhapus!
+                    </div>
 
-                                <meta http-equiv="refresh" content="2, url=kategori.php"/>
+                    <meta http-equiv="refresh" content="2, url=kategori.php" />
                     <?php
-                }else{
+                } else {
                 }
             }
 
 
-        ?>
-    </div>
+            ?>
+        </div>
     </div>
 
     <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
---
+++ admin/produk-detail.php
@@ -0,0 +1,186 @@
+<?php
+require "session.php";
+require "../koneksi.php";
+$id = $_GET['q'];
+
+$query = mysqli_query($con, "SELECT a.*, b.nama AS nama_kategori FROM produk a JOIN kategori b ON a.kategori_id=b.id WHERE  a. id='$id'");
+$data = mysqli_fetch_array($query);
+
+$queryKategori = mysqli_query($con, "SELECT * FROM kategori WHERE id!='$data[kategori_id]'");
+
+function generateRandomString($length = 10)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Produk-Detail</title>
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+</head>
+<style>
+    form div {
+        margin-bottom: 10px;
+    }
+</style>
+
+<body>
+    <?php require "navbar.php"; ?>
+    <div class="container mt-5">
+        <h2>Detail Produk</h2>
+        <div class="col-12 col-md-6">
+            <form action="" method="post" enctype="multipart/form-data">
+                <div>
+                    <label for="nama">Nama</label>
+                    <input type="text" id="nama" name="nama" class="form-control" value="<?php echo $data['nama'] ?>"
+                        autocomplete="off" required>
+                </div>
+                <div>
+                    <label for="kategori">Kategori</label>
+                    <select name="kategori" id="kategori" class="form-control" required>
+                        <option value="<?php echo $data['kategori_id']; ?>">
+                            <?php echo $data['nama_kategori']; ?>
+                        </option>
+                        <?php
+                        while ($dataKategori = mysqli_fetch_array($queryKategori)) {
+                            ?>
+                            <option value="<?php echo $dataKategori['id']; ?>">
+                                <?php echo $dataKategori['nama']; ?>
+                            </option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+                </div>
+                <div>
+                    <label for="harga">Harga</label>
+                    <input type="number" class="form-control" value="<?php echo $data['harga']; ?>" name="harga"
+                        required>
+                </div>
+                <div>
+                    <label for="curent-foto">Foto Produk Sekarang</label><br>
+                    <img src="../image/<?php echo $data['foto']; ?>" width="300px">
+                </div>
+                <div>
+                    <label for="foto">Foto</label><br>
+                    <input type="file" name="foto" id="foto">
+                </div>
+                <div>
+                    <label for="detail">Detail</label>
+                    <textarea name="detail" id="detail" cols="30" rows="10" class="form-control">
+                        <?php echo $data['detail'] ?>
+                    </textarea>
+                </div>
+                <div>
+                    <label for="stok">Stok</label>
+                    <select name="ketersediaan_stok" id="ketersediaan_stok" class="form-control">
+                        <option value="<?php echo $data['ketersediaan_stok'] ?>">
+                            <?php echo $data['ketersediaan_stok'] ?>
+                        </option>
+                        <?php
+                        if ($data['ketersediaan_stok'] == 'ready') {
+                            ?>
+                            <option value="kosong">kosong</option>
+                            <?php
+                        } else {
+                            ?>
+                            <option value="ready">ready</option>
+                            <?php
+                        }
+                        ?>
+                    </select>
+
+                </div>
+                <div class="mt-3">
+                    <button type="submit" class="btn btn-primary" name="simpan">Simpan</button>
+                    <button type="submit" class="btn btn-primary" name="deleteBtn">Delete</button>
+                </div>
+            </form>
+        </div>
+        <?php
+        if (isset($_POST['simpan'])) {
+            $nama = htmlspecialchars($_POST['nama']);
+            $kategori = htmlspecialchars($_POST['kategori']);
+            $harga = htmlspecialchars($_POST['harga']);
+            $detail = htmlspecialchars($_POST['detail']);
+            $ketersediaan_stok = htmlspecialchars($_POST['ketersediaan_stok']);
+
+            $target_dir = "../image/";
+            $nama_file = basename($_FILES["foto"]["name"]);
+            $target_file = $target_dir . $nama_file;
+            $tipe_file = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+            $image_size = $_FILES["foto"]["size"];
+            $randomName = generateRandomString(20);
+            $newName = $randomName . "." . $tipe_file;
+            if ($nama == '' || $kategori == '' || $harga == '') {
+                ?>
+                <div class="alert alert-danger mt-3" role="alert">
+                    Nama Kategori dan Harga Wajib ada!
+                </div>
+                <?php
+            } else {
+                // pembatasan Size File
+                if ($nama_file != '') {
+                    if ($image_size > 500000) {
+                        ?>
+                        <div class="alert alert-danger mt-3" role="alert">
+                            Foto tidak boleh lebih darik 500kb
+                        </div>
+                        <?php
+                    } else {
+                        if ($tipe_file != 'jpg' && $tipe_file != 'png' && $tipe_file != 'gif') {
+                            ?>
+                            <div class="alert alert-danger mt-3" role="alert">
+                                tipe file tidak diperbolehkan
+                            </div>
+                            <?php
+                        } else {
+
+                            move_uploaded_file($_FILES["foto"]["tmp_name"], $target_dir . $newName);
+                        }
+                    }
+
+                }
+                // inseert
+                $queryUPDATE = mysqli_query($con, "UPDATE produk SET kategori_id='$kategori', nama='$nama', harga='$harga', detail='$detail', ketersediaan_stok='$ketersediaan_stok' WHERE id=$id ");
+                if ($queryUPDATE) {
+                    ?>
+                    <div class="alert alert-primary mt-3" role="alert">
+                        Produk berhasil di Simpan!
+                    </div>
+                    <meta http-equiv="refresh" content="2; url=produk.php" />
+                    <?php
+                } else {
+                    echo mysqli_error($con);
+                }
+            }
+        }
+        if (isset($_POST['deleteBtn'])) {
+            $queryDelete = mysqli_query($con, "DELETE  FROM produk WHERE id='$id'");
+            if ($queryDelete) {
+                ?>
+                Terhapus!
+            </div>
+
+            <meta http-equiv="refresh" content="2, url=produk.php" />
+            <?php
+            }
+        }
+        ?>
+    </div>
+    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
