bdbd195846715db26fc11b016356368ca0034b7f
day070 <143053861+day070@users.noreply.github.com>
2023-12-02 16:39:17 +0800

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,22 @@
+<nav class="navbar navbar-expand-lg navbar-dark warna1">
+    <div class="container">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item me-4">
+                    <a class="nav-link" href="../admin">Home</a>
+                </li>
+                <li class="nav-item me-4">
+                    <a class="nav-link" href="kategori.php">Kategori</a>
+                </li>
+                </li>
+                <li class="nav-item me-4">
+                    <a class="nav-link" href="produk.php">Produk</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
