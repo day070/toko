fc771f4dc51a14affbb8e3f57da6a49aac2c6e83
day070 <143053861+day070@users.noreply.github.com>
2023-12-03 14:29:46 +0800

    Update produk-detail.php

--- admin/produk-detail.php
+++ admin/produk-detail.php
@@ -28,7 +28,8 @@ function generateRandomString($length = 10)
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produk-Detail</title>
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 <style>
     form div {
@@ -191,7 +192,7 @@ function generateRandomString($length = 10)
         }
         ?>
     </div>
-    <script src="../bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
