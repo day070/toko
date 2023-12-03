d43459e3f967ec9a7206ff3141b69bf6c8771240
day070 <143053861+day070@users.noreply.github.com>
2023-12-03 14:29:27 +0800

    Update Kecil kecilan

--- admin/login.php
+++ admin/login.php
@@ -5,70 +5,76 @@ require "../koneksi.php";
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css">
 </head>
 <style>
-.main{
-    height:100vh;
-}
-.login-box{
-    width:500px;
-    height:300px;
-    box-sizing:border-box;
-    border-radius:10px;
-}
+    .main {
+        height: 100vh;
+    }
+
+    .login-box {
+        width: 500px;
+        height: 300px;
+        box-sizing: border-box;
+        border-radius: 10px;
+    }
 </style>
+
 <body>
-<div class="main d-flex flex-column justify-content-center align-items-center">
-    <div class="login-box p-5 shadow">
-        <form action="" method="post">
-            <div>
-                <label for="username">Username</label>
-                <input type="text" class="form-control" name="username" id="username" autocomplete="off">
-            </div>
-            <div>
-                <label for="password">Password</label>
-                <input type="password" class="form-control" name="password" id="password">
-            </div>
-            <div>
-                <button class="btn btn-success form-control mt-3" type="submit" name="loginbtn">LOGIN</button>
-            </div>
+    <div class="main d-flex flex-column justify-content-center align-items-center">
+        <div class="login-box p-5 shadow">
+            <form action="" method="post">
+                <div>
+                    <label for="username">Username</label>
+                    <input type="text" class="form-control" name="username" id="username" autocomplete="off">
+                </div>
+                <div>
+                    <label for="password">Password</label>
+                    <input type="password" class="form-control" name="password" id="password">
+                </div>
+                <div>
+                    <button class="btn btn-success form-control mt-3" type="submit" name="loginbtn">LOGIN</button>
+                </div>
 
-        </form>
-    </div>
-    <div class="mt-3" style="width:500px;">
-        <?php
-        if(isset($_POST['loginbtn'])){
-          $username = htmlspecialchars($_POST['username']);
-          $password = htmlspecialchars($_POST['password']);
-
-          $query = mysqli_query($con, "SELECT * FROM users WHERE username ='$username'");
-          $countdata = mysqli_num_rows($query);
-          $data = mysqli_fetch_array($query);
-          if($countdata>0){
-              if(password_verify($password, $data['password'])){
-                  $_SESSION['username'] = $data['username'];
-                  $_SESSION['nama'] = $data['name_usr'];
-                $_SESSION['login'] = true;
-                header('location: ../admin');
-            }else{
-                 ?>
-            <div class="alert alert-danger" role="alert">Password Salah!</div>
+            </form>
+        </div>
+        <div class="mt-3" style="width:500px;">
             <?php
+            if (isset($_POST['loginbtn'])) {
+                $username = htmlspecialchars($_POST['username']);
+                $password = htmlspecialchars($_POST['password']);
+
+                $query = mysqli_query($con, "SELECT * FROM users WHERE username ='$username'");
+                $countdata = mysqli_num_rows($query);
+                $data = mysqli_fetch_array($query);
+                if ($countdata > 0) {
+                    if (password_verify($password, $data['password'])) {
+                        $_SESSION['username'] = $data['username'];
+                        $_SESSION['nama'] = $data['name_usr'];
+                        $_SESSION['login'] = true;
+                        header('location: ../admin');
+                    } else {
+                        ?>
+                        <div class="alert alert-danger" role="alert">Password Salah!</div>
+                        <?php
+                    }
+                } else {
+                    ?>
+                    <div class="alert alert-danger" role="alert">Data yang kamu masukan Salah!</div>
+                    <?php
+                }
             }
-           }else{
-            ?>
-            <div class="alert alert-danger" role="alert">Data yang kamu masukan Salah!</div>
-            <?php
-           }
-        }
 
-        ?>
+            ?>
+        </div>
     </div>
-</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
