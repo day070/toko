baa92e0a171f6c12e97c81af901326eee81ee176
day070 <143053861+day070@users.noreply.github.com>
2023-12-03 14:37:22 +0800

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -7,7 +7,7 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item me-4">
-                    <a class="nav-link" href="../admin">Home</a>
+                    <a class="nav-link" href="index.php">Home</a>
                 </li>
                 <li class="nav-item me-4">
                     <a class="nav-link" href="tentang-kami.php">Tentang Kami</a>
